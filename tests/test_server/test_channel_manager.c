865f55080f0ddc4ac2880d76ad77073e8b16f61c
Bastien Hugon <bastien.hugon@epitech.eu>
2018-05-27 21:45:08 +0200

    ADD Unit Tests Explode / Channel Management

--- tests/test_server/test_channel_manager.c
+++ tests/test_server/test_channel_manager.c
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2018
 ** TU
 ** File description:
-** Error Handlinig
+** Error Handling
 */
 
 #include "criterion/criterion.h"
@@ -11,6 +11,15 @@
 Test(channel_manager, channel_manager_1)
 {
 	server_t srv;
+	chan_t *chan;
+	users_t *usr = create_user();
 
-	srv = init_server(&srv, 4242);
+	srand(time(0));
+	usr->fd = 12;
+	init_server(&srv, rand() % 4242 + 4242);
+	chan = create_and_add_chan(&srv, "#toto");
+	cr_assert(!strcmp(chan->name, "#toto"), "Bad channel management");
+	(&srv)->chan->users = usr;
+	join_channel(&srv, 12, "#toto");
+	cr_assert(is_on_chan((&srv)->chan, 12), "Bad channel management");
 }
