a71790f61f110c86666cddc488e5b2d18cd82cf6
Bastien Hugon <bastien.hugon@epitech.eu>
2018-05-27 20:51:35 +0200

    UPDATE coding style remastered

--- tests/test_server/test_channel_manager.c
+++ tests/test_server/test_channel_manager.c
@@ -10,7 +10,24 @@
 
 Test(channel_manager, channel_manager_1)
 {
-	server_t srv;
+	chan_t *chan = create_channel("#toto");
 
-	srv = init_server(&srv, 4242);
+	cr_assert(!strcmp("#toto", chan->name), "Bad channel management");
+}
+
+Test(channel_manager, channel_manager_2)
+{
+	server_t *srv;
+	chan_t *chan;
+
+	chan = create_and_add_chan("#toto");
+	init_server(&srv, 4242);
+	cr_assert(!strcmp("#toto", chan->name), "Bad channel management");
+}
+
+Test(channel_manager, channel_manager_3)
+{
+	server_t *srv;
+
+	init_server(&srv, 4242);
 }
